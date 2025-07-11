baa857eb25faead64aec743ea9e82bfdb876a901
Cynthia Spinner <CynthiaSpinner@gmail.com>
2025-07-11 16:56:32 -0500

    Trigger frontend deployment with static file serving fix

--- ClientApp/src/App.js
+++ ClientApp/src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
-// Frontend deployment test - permissions fixed
+// Frontend deployment test - static file serving fix
 import { PortfolioProvider } from "./context/PortfolioContext";
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
