05323564e86d636692f5691b7a3fa46634a5eb81
Cynthia Spinner <CynthiaSpinner@gmail.com>
2025-07-11 16:23:26 -0500

    Trigger frontend deployment with ClientApp changes

--- ClientApp/src/App.js
+++ ClientApp/src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
-// Frontend deployment test
+// Frontend deployment test - debugging Azure App Service
 import { PortfolioProvider } from "./context/PortfolioContext";
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
